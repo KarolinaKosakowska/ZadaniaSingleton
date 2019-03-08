49c59446dd873621e7cee2dfce4676b71a59481e
KarolinaKosakowska <karolina.kosakowska.email@gmail.com>
2019-03-08 11:41:36 +0100

    Zadanie 1 completed

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Roman rozpoczyna pracę");
+            var roman = RomanEmployee.Start();
+            Console.WriteLine($"Pracownik jest w pracy: {RomanEmployee.IsWorking()}");
+            Console.WriteLine("Roman kończy pracę");
+            RomanEmployee.Stop();
+            Console.WriteLine($"Pracownik jest w pracy: {RomanEmployee.IsWorking()}");
+            Console.ReadKey();
+        }
+    }
+    
+}
---
+++ RomanEmployee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class RomanEmployee
+    {
+        private static RomanEmployee instance;
+        protected RomanEmployee() { }
+        public static RomanEmployee Instance()
+        {
+            if(instance==null)
+            {
+                instance = new RomanEmployee();
+            }
+            return instance;
+        }
+       
+        public static bool IsWorking()
+        {
+            if (instance != null)
+                return true;
+            else
+                return false;            
+        }
+        public static RomanEmployee Start()
+        {
+            RomanEmployee.Instance();
+            return instance;
+        }
+
+        public static void Stop()
+        {
+            instance = null;
+        }
+    }
+}
